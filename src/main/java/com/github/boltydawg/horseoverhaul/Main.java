1a256c6d61297f62df7b9572a69f1330bb45e6bf
BoltyDawg <boltydawg@gmail.com>
2019-05-24 23:35:11 -0400

    added functionality for checking the stats of your own horse

--- src/main/java/com/github/boltydawg/horseoverhaul/Main.java
+++ src/main/java/com/github/boltydawg/horseoverhaul/Main.java
@@ -14,6 +14,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 //TODO add feature for viewing any horse's stats
 //TODO re-work horse breeding algorithm
 //TODO improve printHorseStats(), add symbols
+//TODO food type specific breeding
+//TODO name horse upon birth
 
 public class Main extends JavaPlugin{
 	public static DecimalFormat df = new DecimalFormat("0.00");
@@ -26,18 +28,22 @@ public class Main extends JavaPlugin{
 		
 		config = this.getConfig();
 		
-		config.addDefault("betterBreeding", true);
 		config.addDefault("autoSaddleMount", true);
+		config.addDefault("betterBreeding", true);
+		config.addDefault("checkHorseStats", true);
 		config.addDefault("dropHorseGear", true);
 		
 		config.options().copyDefaults(true);
 		saveConfig();
 		
+		if(config.getBoolean("autoSaddleMount"))
+			this.getServer().getPluginManager().registerEvents(new ListenerSaddle(), this);
+		
 		if(config.getBoolean("betterBreeding"))
 			this.getServer().getPluginManager().registerEvents(new FoalListener(), this);
 		
-		if(config.getBoolean("autoSaddleMount"))
-			this.getServer().getPluginManager().registerEvents(new ListenerSaddle(), this);
+		if(config.getBoolean("checkHorseStats"))
+			this.getServer().getPluginManager().registerEvents(new ListenerStats(), this);
 		
 		if(config.getBoolean("dropHorseGear"))
 			this.getServer().getPluginManager().registerEvents(new ListenerHorseDeath(), this);
