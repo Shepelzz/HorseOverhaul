94f1475435dccfca4f8ace42f15b73478a0bda15
BoltyDawg <boltydawg@gmail.com>
2020-02-12 03:05:11 -0500

    rework breeding algorithm

--- src/main/java/com/github/boltydawg/horseoverhaul/StatHorse.java
+++ src/main/java/com/github/boltydawg/horseoverhaul/StatHorse.java
@@ -11,34 +11,48 @@ import org.bukkit.entity.LivingEntity;
  * @see https://minecraft.gamepedia.com/Horse
  */
 public class StatHorse{
+	//fields
 	Horse roach;
 	byte food;
 	
+	//constructors
 	public StatHorse(Horse horse) {
 		roach = horse;
 		food = 0;
 	}
+	
 	public StatHorse(LivingEntity horse) {
 		roach = (Horse)horse;
 		food = 0;
 	}
+	
 	public StatHorse(LivingEntity horse, byte f) {
 		roach = (Horse)horse;
-		food = f;
+		if(Main.config.getBoolean("betterBreedingFoodMultipliers") && Main.config.getBoolean("betterBreeding"))
+			food = f;
+		else
+			food = 0;
 	}
 	
+	
+	//get methods
 	public double getJumpHeight() {
 		double x = roach.getJumpStrength();
 		
 		return -0.1817584952 * Math.pow(x, 3) + 3.689713992 * Math.pow(x, 2) + 2.128599134 * x - 0.343930367;
 	}
+	
 	public int getHealth() {
 		return (int)(roach.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue()/2);
 	}
+	
 	public double getSpeed() {
 		double x = roach.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue();
 		return 43.178 * x - 0.02141;
 	}
+	
+	
+	//public methods
 	public String printStats(boolean border) {
 		String msg = "";
 		
@@ -60,6 +74,33 @@ public class StatHorse{
 		else
 			return msg;
 	}
+	
+	public void calculateBirth(Horse mother, Horse father) {
+		if(food == (byte)2) {
+			roach.setJumpStrength(1.0);
+			roach.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(30);
+			roach.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.3375);
+		}
+		double fj, fh, fs, mj, mh, ms;
+		double boost = 1.0;
+		
+		if(food == (byte)1)
+			boost = 1.10;
+		
+		fj = father.getJumpStrength();
+		fh = father.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
+		fs = father.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue();
+		
+		mj = mother.getJumpStrength();
+		mh = mother.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
+		ms = mother.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue();
+		
+		roach.setJumpStrength(boost * calcJump(fj, mj));
+		roach.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(boost*calcHealth(fh, mh));
+		roach.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(boost*calcSpeed(fs, ms));
+	}
+	
+	//printing methods
 	private String printJump(double jh) {
 		String msg = "";
 		double b = 0;
@@ -117,79 +158,98 @@ public class StatHorse{
 		msg += ChatColor.GRAY + hearts;
 		return msg;
 	}
-	public void calculateBirth(Horse mother, Horse father) {
-		double fj, fh, fs, mj, mh, ms;
-		
-		fj = father.getJumpStrength();
-		fh = father.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
-		fs = father.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue();
-		
-		mj = mother.getJumpStrength();
-		mh = mother.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
-		ms = mother.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue();
-		
-		roach.setJumpStrength(calcJump(fj, mj));
-		roach.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(calcHealth(fh, mh));
-		roach.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(calcSpeed(fs, ms));
-	}
+	
+	
 	/**
-	 * Horse Jump height can range from 0.4-1.0, average is 0.7
-	 * TODO deal with golden apples
+	 * 
+	 * @param f: mother's stat, on scale of 0-1.0
+	 * @param m: father's stat, on scale of 0-1.0
+	 * @return a double on scale of 0-1.0 that will determine the child's attribute
+	 */
+	private double randomizer(double f, double m) {
+		double min = f < m ? f : m;
+		double max = f > m ? f : m;
+		
+		if(food == 0)
+			min = min * (Math.random() * 0.3 + 0.9);
+		
+		if(max > .8)
+			max += (1-max)/1.5;
+		else
+			max = max * (Math.random() * 0.25 + 0.9);
+		
+		return Math.random() * (max-min) + min;
+//		double mrand = (1+(1-m)) * m * (Math.random() * 0.1 + .95);
+//		double frand = (1+(1-f)) * f * (Math.random() * 0.1 + .95);
+//		
+//		double ret = (mrand + frand) / 2;
+//		
+//		if(ret > 1.0)
+//			return 1.0;
+//		return ret;
+	}
+	
+	/**
+	 * Breeding calculations.
+	 * Notes:
+	 * 	Horse Jump height can range from 0.4-1.0, average is 0.7
+	 * 	TODO deal with golden apples
 	 */
 	private double calcJump(double f, double m) {
-		if(food == 2) {
-			return 1;
-		}
-		double x, bound, origin;
+//		double x, bound, origin;
+//		
+//		if(f > m) {
+//			//bound = 1 - ((1 - f) / 3);
+//			if(f >= .8)
+//				bound = f + 0.075;
+//			else
+//				bound = f + 0.1;
+//			origin = m;
+//		}
+//		else {
+//			//bound = 1 - ((1 - m) / 3);
+//			if(m >= .8)
+//				bound = m + 0.075;
+//			else
+//				bound = m + 0.1;
+//			origin = f;
+//		}
+//		if(bound > 1)
+//			bound = 1;
+//		
+//		x = Math.random() * (bound - origin) + origin;
+//
+//		return x;
 		
-		if(f > m) {
-			//bound = 1 - ((1 - f) / 3);
-			if(f >= .8)
-				bound = f + 0.075;
-			else
-				bound = f + 0.1;
-			origin = m;
-		}
-		else {
-			//bound = 1 - ((1 - m) / 3);
-			if(m >= .8)
-				bound = m + 0.075;
-			else
-				bound = m + 0.1;
-			origin = f;
-		}
-		if(bound > 1)
-			bound = 1;
+		double mc = (m - .4)  / 0.6;
+		double fc = (f - .4) / 0.6;
 		
-		x = Math.random() * (bound - origin) + origin;
-
-		return x;
+		double child = randomizer(mc, fc);
+		return child * 0.6 + 0.4;
 	}
 	
 	/**
 	 * Health can range from 15-30, average is 22-23
 	 */
 	private double calcHealth(double f, double m) {
-		if(food == 2) {
-			return 30;
-		}
 		 
-		double x = (f + m) / 2;
+		double mc = (m - 15) / 15;
+		double fc = (f - 15) / 15;
 		
-		return x;
+		double child = randomizer(mc, fc);
+		return child * 15 + 15;
 	}
 	
 	/**
 	 * ranges from 0.1125 - 0.3375, average is 0.225
 	 */
 	private double calcSpeed(double f, double m) {
-		if(food == 2) {
-			return 0.3375;
-		}
 		
-		double x = (f + m) / 2;
+		double mc = (m - 0.1125) / 0.225;
+		double fc = (f - 0.1125) / 0.225;
 		
-		return x;
+		double child = randomizer(mc, fc);
+		return child * 0.225 + 0.1125;
 	}
 	
 }
