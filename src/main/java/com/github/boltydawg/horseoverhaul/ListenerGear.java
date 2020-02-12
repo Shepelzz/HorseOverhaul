15ef58f66f91570e1e2238dda35dfb8ecfbb2a75
BoltyDawg <boltydawg@gmail.com>
2020-02-12 00:53:15 -0500

    replace autoSaddleMount with autoGearEquip

---
+++ src/main/java/com/github/boltydawg/horseoverhaul/ListenerGear.java
@@ -0,0 +1,31 @@
+package com.github.boltydawg.horseoverhaul;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Horse;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class ListenerGear implements Listener {
+	
+	@EventHandler
+	public void clickEntity(PlayerInteractEntityEvent event) {
+		if(event.getRightClicked() instanceof Horse) {
+			Horse horse = (Horse)event.getRightClicked();
+			if(!event.getPlayer().isSneaking()) {
+				ItemStack hand = event.getPlayer().getInventory().getItemInMainHand();
+				if( hand.getType().equals(Material.SADDLE) && horse.getInventory().getSaddle() == null ) {
+					horse.getInventory().setSaddle(hand);
+					event.getPlayer().getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+					event.setCancelled(true);
+				}
+				else if(hand.getType().name().contains("HORSE_ARMOR") && horse.getInventory().getArmor() == null) {
+					horse.getInventory().setArmor(hand);
+					event.getPlayer().getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+					event.setCancelled(true);
+				}
+			}
+		}
+	}
+}
