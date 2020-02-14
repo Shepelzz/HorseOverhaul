8806cb0e90aad104ea2454d1e1e6d4e36489d47e
BoltyDawg <boltydawg@gmail.com>
2020-02-14 02:33:20 -0500

    stat storage

--- src/main/java/com/github/boltydawg/horseoverhaul/Listeners/StatsListener.java
+++ src/main/java/com/github/boltydawg/horseoverhaul/Listeners/StatsListener.java
@@ -1,26 +1,44 @@
 package com.github.boltydawg.horseoverhaul.Listeners;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.bukkit.Material;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.github.boltydawg.horseoverhaul.StatHorse;
+import com.github.boltydawg.horseoverhaul.Main;
 
 public class StatsListener implements Listener {
 	
 	/**
 	 * This class holds the listener that triggers when a player right clicks a horse
-	 * 	with the intention of checking its stats.
+	 * 	with the intention of checking its stats, and the necessary listeners to
+	 *  allow players to store the stats of the last horse they inspected to a sign.
+	*/
+	 
+	 
+	 
+	/**
 	 * @param event - event triggered when a player right clicks a horse
 	 * 
 	 * The priority is set to low because this same event is handled by the ownership
 	 * 	listener, and if the ownership listener wants to cancel the event, it needs to
 	 * 	do so before this event runs
 	 */
+	
+	public static HashMap<UUID, ArrayList<String>> signStats = new HashMap<UUID, ArrayList<String>>();
+	
 	@EventHandler(priority = EventPriority.LOW)
 	public void onClick(PlayerInteractEntityEvent event) {
 		
@@ -38,7 +56,57 @@ public class StatsListener implements Listener {
 				event.setCancelled(true);
 				player.sendMessage(new StatHorse(horse).printStats(true));
 				
+				
+				ArrayList<String> stats = new ArrayList<String>();
+				StatHorse roach = new StatHorse(horse);
+				
+				stats.add(horse.getCustomName() + ":");
+				stats.add("Health: " + roach.getHealth());
+				stats.add("Speed: " + Main.df.format(roach.getSpeed()));
+				stats.add("Jump: " + Main.df.format(roach.getJumpHeight()));
+				
+				signStats.put(player.getUniqueId(), stats);
+				
 			}
 		}
+	}
+	
+	/**
+	 * Detects when a player right clicks a sign while holding a carrot on a stick
+	 * @param event
+	 */
+	@EventHandler
+	public void onClick(PlayerInteractEvent event) {
+		if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getType().name().contains("_SIGN")
+				&& event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.CARROT_ON_A_STICK) && signStats.containsKey(event.getPlayer().getUniqueId())) {
+			
+			Sign sign = (Sign)(event.getClickedBlock().getState());
+			
+			ArrayList<String> msg = signStats.get(event.getPlayer().getUniqueId());
+			
+			for(int i=0; i<4; i++) {
+				
+				sign.setLine(i, msg.get(i));
+				event.getPlayer().sendMessage(sign.getLine(i));
+
+			}
+			
+			sign.update();
+			
+			signStats.remove(event.getPlayer().getUniqueId());
+			
+		}	
+	}
+	
+	
+	/**
+	 * Removes a player's storage when they leave
+	 * @param event
+	 */
+	@EventHandler
+	public void onLeave(PlayerQuitEvent event) {
+		
+		signStats.remove(event.getPlayer().getUniqueId());
+		
 	}
 }
