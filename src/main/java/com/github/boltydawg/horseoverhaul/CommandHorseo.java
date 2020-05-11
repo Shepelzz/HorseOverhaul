bafffcfa6af711ce391a5ed9ca04032f7602d889
BoltyDawg <boltydawg@gmail.com>
2020-05-11 07:01:54 -0400

    add missing info to ownership help

--- src/main/java/com/github/boltydawg/horseoverhaul/CommandHorseo.java
+++ src/main/java/com/github/boltydawg/horseoverhaul/CommandHorseo.java
@@ -91,10 +91,10 @@ public class CommandHorseo implements CommandExecutor {
 		if( OwnershipListener.ownership ) {
 			
 			if(OwnershipListener.craftDeed) {
-				msg += ChatColor.GREEN + "Craft a deed by combining a golden carrot with a book and quill.\n" + ChatColor.RESET;
+				msg += ChatColor.GREEN + "Craft a blank deed by combining a golden carrot with a book and quill.\n" + ChatColor.RESET;
 			}
 			
-			msg += ChatColor.YELLOW + "Claim a horse as yours by right clicking it with a deed!\n" + ChatColor.RESET;
+			msg += ChatColor.YELLOW + "Claim a horse as yours by right clicking it with a blank deed, or if its already claimed, the original copy of its deed!\n" + ChatColor.RESET;
 			msg += ChatColor.GREEN + "Other players cannot interact with horses that you own, and equipping your owned horses with armor prevents you from accidentally damaging them :)\n" + ChatColor.RESET;
 			msg += ChatColor.YELLOW + "If you're in the business of selling horses, you can right click a baby foal that you own with shears to neuter it and prevent it from ever breeding.\n" + ChatColor.RESET;
 			msg += ChatColor.GREEN + "If trying to use a nametag on one of your horses, you must be holding its deed in your off hand.\n" + ChatColor.RESET;
