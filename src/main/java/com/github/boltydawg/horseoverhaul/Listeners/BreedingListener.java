e98e367599442c483b58cbb92dfc74a35ddd18f2
BoltyDawg <boltydawg@gmail.com>
2020-12-07 01:52:17 -0500

    add breeding support for all AbstractHorses

--- src/main/java/com/github/boltydawg/horseoverhaul/Listeners/BreedingListener.java
+++ src/main/java/com/github/boltydawg/horseoverhaul/Listeners/BreedingListener.java
@@ -2,8 +2,7 @@ package com.github.boltydawg.horseoverhaul.Listeners;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Horse;
+import org.bukkit.entity.AbstractHorse;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,20 +17,20 @@ public class BreedingListener implements Listener{
 	@EventHandler
 	public void onBreed(EntityBreedEvent event) {
 		
-		if(event.getEntityType().equals(EntityType.HORSE) && event.getBreeder() instanceof Player) {
+		if(event.getEntity() instanceof AbstractHorse && event.getBreeder() instanceof Player) {
 			
-			//ensures that the two parents are horses
-			if(!(event.getFather() instanceof Horse && event.getMother() instanceof Horse)) return;
+			// ensures that the two parents are horses
+			if(!( event.getFather() instanceof AbstractHorse && event.getMother() instanceof AbstractHorse )) return;
 			
 			Player player = (Player)event.getBreeder();
 			
-			//handle if either of the parents are neutered
+			// handle if either of the parents are neutered
 			boolean fneut = event.getFather().getScoreboardTags().contains("ho.isNeutered");
 			boolean mneut = event.getMother().getScoreboardTags().contains("ho.isNeutered");
 			if( fneut || mneut ) {
 				
-				Horse father = (Horse)event.getFather();
-				Horse mother = (Horse)event.getMother();
+				AbstractHorse father = (AbstractHorse)event.getFather();
+				AbstractHorse mother = (AbstractHorse)event.getMother();
 				
 				String r = ChatColor.RESET + ChatColor.RED.toString();
 				if ( fneut && mneut ) {
@@ -51,29 +50,24 @@ public class BreedingListener implements Listener{
 				
 			}
 			
-			//set the stats based off what what breeding food was used
+			// set the stats based off what what breeding food was used
 			StatHorse foal;
 			if(event.getBredWith().getType().equals(Material.ENCHANTED_GOLDEN_APPLE)) {
 				
-				foal = new StatHorse(event.getEntity(), (byte)2);
+				foal = new StatHorse((AbstractHorse)event.getEntity(), (byte)2);
 				event.getEntity().getScoreboardTags().add("ho.isNeutered");
 				
 			}
 
 			else if(event.getBredWith().getType().equals(Material.GOLDEN_APPLE))
-				foal = new StatHorse(event.getEntity(), (byte)1);
+				foal = new StatHorse((AbstractHorse)event.getEntity(), (byte)1);
 			else
-				foal = new StatHorse(event.getEntity());
+				foal = new StatHorse((AbstractHorse)event.getEntity());
 			
 			
-			foal.calculateBirth((Horse)event.getMother(),(Horse)event.getFather());
+			foal.calculateBirth((AbstractHorse)event.getMother(),(AbstractHorse)event.getFather());
 			foal.roach.setTamed(true);
 			
-//			StatHorse father = new StatHorse(event.getFather());
-//			StatHorse mother = new StatHorse(event.getMother());
-//			player.sendMessage(foal.printStats("Foal"));
-//			player.sendMessage(father.printStats("Father"));
-//			player.sendMessage(mother.printStats("Mother"));
 		}
 	}
 }
