3c1804f2a4ab1b69a97c6232663e3f30e0322a25
BoltyDawg <boltydawg@gmail.com>
2020-12-08 04:49:13 -0500

    establish JUnit testing framework

---
+++ src/test/java/com/github/boltydawg/AppTest.java
@@ -0,0 +1,18 @@
+package com.github.boltydawg;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class AppTest {
+	
+	public static void main(String[] args) {
+		
+		Result result = JUnitCore.runClasses(OwnershipTests.class, BreedingTests.class);
+		
+		for (Failure failure : result.getFailures()) {
+			System.out.println(failure.toString());
+		}
+	}
+	
+}
---
+++ src/test/java/com/github/boltydawg/OwnershipTests.java
@@ -0,0 +1,14 @@
+package com.github.boltydawg;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OwnershipTests {
+	
+	@Test
+	public void trivialTest() {
+		boolean t = true;
+		assertTrue(t);
+	}
+}
