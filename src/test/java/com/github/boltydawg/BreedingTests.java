76642d1cd2b27779ed9a322a4521edf123c08f4f
BoltyDawg <boltydawg@gmail.com>
2020-12-08 04:49:13 -0500

    establish JUnit testing framework

---
+++ src/test/java/com/github/boltydawg/BreedingTests.java
@@ -0,0 +1,14 @@
+package com.github.boltydawg;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BreedingTests {
+	
+	@Test
+	public void trivialTest() {
+		boolean t = true;
+		assertTrue(t);
+	}
+}
