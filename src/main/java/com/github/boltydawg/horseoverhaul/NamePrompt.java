1656a1e66ad4fc6c20b5873d16a19a2943c19b65
BoltyDawg <boltydawg@gmail.com>
2021-01-28 17:37:23 -0500

    add clickable link to color codes if coloredNames is enabled

--- src/main/java/com/github/boltydawg/horseoverhaul/NamePrompt.java
+++ src/main/java/com/github/boltydawg/horseoverhaul/NamePrompt.java
@@ -1,6 +1,5 @@
 package com.github.boltydawg.horseoverhaul;
 
-import org.bukkit.ChatColor;
 import org.bukkit.conversations.ConversationContext;
 import org.bukkit.conversations.Prompt;
 import org.bukkit.conversations.StringPrompt;
@@ -8,6 +7,10 @@ import org.bukkit.entity.AbstractHorse;
 import org.bukkit.entity.Player;
 
 import com.github.boltydawg.horseoverhaul.Listeners.OwnershipListener;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class NamePrompt extends StringPrompt {
 	
@@ -22,8 +25,31 @@ public class NamePrompt extends StringPrompt {
 	@Override
 	public String getPromptText(ConversationContext context) {
 		
-		return ChatColor.BLUE + "What would you like to name your new steed?" + ChatColor.RESET + '\n' + ChatColor.GRAY + "(type your answer in chat)";
+		TextComponent tc = new TextComponent("What would you like to name your new steed?");
+		tc.setColor(ChatColor.BLUE);
 		
+		if(OwnershipListener.coloredNames) {
+			TextComponent tc1 = new TextComponent("\nClick ");
+			tc1.setColor(ChatColor.BLUE);
+			
+			TextComponent tc2 = new TextComponent("here");
+			tc2.setColor(ChatColor.GOLD);
+			tc2.setUnderlined(true);
+			tc2.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.spigotmc.org/attachments/example2-png.188806"));
+			
+			TextComponent tc3 = new TextComponent(" to see a list of available color codes");
+			tc3.setColor(ChatColor.BLUE);
+			
+			tc.addExtra(tc1);
+			tc.addExtra(tc2);
+			tc.addExtra(tc3);
+		}
+		
+		if(context.getForWhom() instanceof Player) {
+			((Player)context.getForWhom()).spigot().sendMessage(tc);
+		}
+		
+		return ChatColor.GRAY + "(type your answer in chat)";
 	}
 
 	@Override
